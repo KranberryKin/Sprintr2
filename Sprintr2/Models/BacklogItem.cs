9caf1b6a29f885bb4a450454b85f55632795a7b8
KranberryKin <quinnclimbs@gmail.com>
2021-11-24 16:12:44 -0700

    Database tables set up

--- Sprintr2/Models/BacklogItem.cs
+++ Sprintr2/Models/BacklogItem.cs
@@ -5,7 +5,7 @@ namespace Sprintr2.Models
     public int Id { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
-    public string Enum { get; set; }
+    public string Status { get; set; }
     public int ProjectId { get; set; }
     public int? SprintId { get; set; }
     public string CreatorId { get; set; }
--- Sprintr2/dbSetup.sql
+++ Sprintr2/dbSetup.sql
@@ -7,11 +7,72 @@ CREATE TABLE IF NOT EXISTS accounts(
   picture varchar(255) COMMENT 'User Picture'
 ) default charset utf8 COMMENT '';
 
+
+CREATE TABLE IF NOT EXISTS projects(
+  id int NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT 'primary key',
+  createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
+  updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
+  name VARCHAR(255),
+  description VARCHAR(255),
+  creatorId VARCHAR(255) NOT NULL,
+  FOREIGN KEY (creatorId) REFERENCES accounts(id)
+) default charset utf8 COMMENT '';
+
+
+CREATE TABLE IF NOT EXISTS sprints(
+  id int NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT 'primary key',
+  name VARCHAR(255) NOT NULL,
+  createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
+  updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
+  startDate DATETIME,
+  endDate DATETIME,
+  projectId int,
+  creatorId VARCHAR(255) NOT NULL,
+  isOpen TINYINT NOT NULL DEFAULT 1,
+  FOREIGN KEY (projectId) REFERENCES projects(id),
+  FOREIGN KEY (creatorId) REFERENCES accounts(id)
+) default charset utf8 COMMENT '';
 CReATE TABLE IF NOT EXISTS backlogitems(
   id int NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT 'primary key',
   createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
   updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
   name VARCHAR(255),
   description VARCHAR(255),
-  status enum
-)
+  status VARCHAR(255) NOT NULL DEFAULT 'Pending',
+  projectId int NOT NULL,
+  sprintId int,
+  creatorId VARCHAR(255) NOT NULL,
+  FOREIGN KEY (projectId) REFERENCES projects(id),
+  FOREIGN KEY (sprintId) REFERENCES sprints(id),
+  FOREIGN Key (creatorId) REFERENCES accounts(id)
+) default charset utf8 COMMENT '';
+
+
+CREATE TABLE IF NOT EXISTS notes(
+  id int NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT 'primary key',
+  createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
+  updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
+  body VARCHAR(255),
+  backlogitemId int,
+  projectId int,
+  creatorId VARCHAR(255),
+  FOREIGN KEY (backlogitemId) REFERENCES backlogitems(id),
+  FOREIGN KEY (projectId) REFERENCES projects(id),
+  FOREIGN KEY (creatorId) REFERENCES accounts(id)
+) default charset utf8 COMMENT '';
+
+
+CREATE TABLE IF NOT EXISTS tasks(
+  id int NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT 'primary key',
+  createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
+  updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
+  name VARCHAR(255),
+  weight int,
+  projectId int,
+  backlogitemId int,
+  creatorId VARCHAR(255),
+  isComplete TINYINT NOT NULL DEFAULT 0,
+  FOREIGN KEY (projectId) REFERENCES projects(id),
+  FOREIGN KEY (backlogitemId) REFERENCES backlogitems(id),
+  FOREIGN KEY (creatorId) REFERENCES accounts(id)
+) default charset utf8 COMMENT '';
